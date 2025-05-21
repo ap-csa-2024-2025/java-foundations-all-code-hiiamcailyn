1e1a957aab6918e349da50cb30f8e9a703dffae7
Cailyn Tran <152474332+hiiamcailyn@users.noreply.github.com>
2025-05-21 17:21:03 +0000

    chicken

--- src/jfo-section-3/Chickens01.java
+++ src/jfo-section-3/Chickens01.java
@@ -1,6 +1,20 @@
 public class Chickens01 {
     public static void main(String[] args) {
-        //Put yout code here
+        int totalEggs = 0; 
+        int chickenCount = 3;
+        int eggsPerChicken = 5; 
+
+        //Monday
+        totalEggs += chickenCount * eggsPerChicken;
+
+        //Tuesday
+       
+        totalEggs += ++chickenCount *eggsPerChicken;
+
+        //Wednesday 
+        chickenCount /= 2;
+        totalEggs += totalEggs * chickenCount + eggsPerChicken;
+
         
         System.out.println(totalEggs);
     }   
